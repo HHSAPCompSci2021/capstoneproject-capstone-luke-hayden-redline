dfb78cbc7b98cfa932c813060d78a6857045b458
lma425 <lma425@student.fuhsd.org>
2022-05-18 19:24:13 -0700

    Update Track.java

--- CapstoneProject/src/gameplay/Track.java
+++ CapstoneProject/src/gameplay/Track.java
@@ -5,14 +5,13 @@ import processing.core.PApplet;
 /**
  * 
  * @author Luke Ma, Hayden Kaplanov
- * @version 1
- * Represents a racetrack
+ * @version 1 Represents a racetrack
  */
 public class Track extends Shape {
 	private double innerRadius;
 	private double outerRadius;
 	private double straightLength;
-	
+
 	public Track(double x, double y, double innerRadius, double outerRadius, double straightLength) {
 		super(x, y);
 		this.innerRadius = innerRadius;
@@ -22,44 +21,48 @@ public class Track extends Shape {
 
 	@Override
 	public void draw(PApplet drawer) {
-		drawer.arc((float)x, (float)y, (float)outerRadius*2, (float)outerRadius*2, PApplet.HALF_PI, PApplet.PI+PApplet.HALF_PI);
-		drawer.arc((float)x, (float)y, (float)innerRadius*2, (float)innerRadius*2, PApplet.HALF_PI, PApplet.PI+PApplet.HALF_PI);
-		
-		drawer.arc((float)(x+straightLength), (float)y, (float)outerRadius*2, (float)outerRadius*2, -PApplet.PI+PApplet.HALF_PI, PApplet.HALF_PI);
-		drawer.arc((float)(x+straightLength), (float)y, (float)innerRadius*2, (float)innerRadius*2, -PApplet.PI+PApplet.HALF_PI, PApplet.HALF_PI);
-		
-		drawer.line((float)x, (float)(y-innerRadius), (float)(x+straightLength), (float)(y-innerRadius));
-		drawer.line((float)x, (float)(y-outerRadius), (float)(x+straightLength), (float)(y-outerRadius));
-		drawer.line((float)x, (float)(y+innerRadius), (float)(x+straightLength), (float)(y+innerRadius));
-		drawer.line((float)x, (float)(y+outerRadius), (float)(x+straightLength), (float)(y+outerRadius));
-		
+		drawer.arc((float) x, (float) y, (float) outerRadius * 2, (float) outerRadius * 2, PApplet.HALF_PI,
+				PApplet.PI + PApplet.HALF_PI);
+		drawer.arc((float) x, (float) y, (float) innerRadius * 2, (float) innerRadius * 2, PApplet.HALF_PI,
+				PApplet.PI + PApplet.HALF_PI);
+
+		drawer.arc((float) (x + straightLength), (float) y, (float) outerRadius * 2, (float) outerRadius * 2,
+				-PApplet.PI + PApplet.HALF_PI, PApplet.HALF_PI);
+		drawer.arc((float) (x + straightLength), (float) y, (float) innerRadius * 2, (float) innerRadius * 2,
+				-PApplet.PI + PApplet.HALF_PI, PApplet.HALF_PI);
+
+		drawer.line((float) x, (float) (y - innerRadius), (float) (x + straightLength), (float) (y - innerRadius));
+		drawer.line((float) x, (float) (y - outerRadius), (float) (x + straightLength), (float) (y - outerRadius));
+		drawer.line((float) x, (float) (y + innerRadius), (float) (x + straightLength), (float) (y + innerRadius));
+		drawer.line((float) x, (float) (y + outerRadius), (float) (x + straightLength), (float) (y + outerRadius));
+
 	}
 
 	@Override
-	public boolean isPointOn(double x, double y) { //test for corners
-		 if(x < this.x-straightLength/2-outerRadius)
-		  return false;
-		 else if(x > this.x+straightLength/2+outerRadius)
-		  return false;
-		 else if(x > this.x-straightLength/2 && x < this.x+straightLength/2) {
-		  if(y<this.y-outerRadius)
-		   return false;
-		  else if(y>this.y+outerRadius)
-		   return false;
-		  else if(y>this.y-innerRadius && y<this.y+innerRadius)
-		   return false;
-		 }
-		 else if(x<this.x-straightLength/2 && x>this.x-straightLength/2-outerRadius) {
-			 double distSquare = (x-(this.x-straightLength/2))*(x-(this.x-straightLength/2))+(y-this.y)*(y-this.y);
-			 if(distSquare>outerRadius*outerRadius || distSquare<innerRadius*innerRadius)
-				 return false;
-		 } else if(x>this.x+straightLength/2 && x<this.x+straightLength/2+outerRadius) {
-			 double distSquare = (x-(this.x+straightLength/2))*(x-(this.x+straightLength/2))+(y-this.y)*(y-this.y);
-			 if(distSquare>outerRadius*outerRadius || distSquare<innerRadius*innerRadius)
-				 return false;
-		 }
-		
+	public boolean isPointOn(double x, double y) { // test for corners
+		if (x < this.x - straightLength / 2 - outerRadius)
+			return false;
+		else if (x > this.x + straightLength / 2 + outerRadius)
+			return false;
+		else if (x > this.x - straightLength / 2 && x < this.x + straightLength / 2) {
+			if (y < this.y - outerRadius)
+				return false;
+			else if (y > this.y + outerRadius)
+				return false;
+			else if (y > this.y - innerRadius && y < this.y + innerRadius)
+				return false;
+		} else if (x < this.x - straightLength / 2 && x > this.x - straightLength / 2 - outerRadius) {
+			double distSquare = (x - (this.x - straightLength / 2)) * (x - (this.x - straightLength / 2))
+					+ (y - this.y) * (y - this.y);
+			if (distSquare > outerRadius * outerRadius || distSquare < innerRadius * innerRadius)
+				return false;
+		} else if (x > this.x + straightLength / 2 && x < this.x + straightLength / 2 + outerRadius) {
+			double distSquare = (x - (this.x + straightLength / 2)) * (x - (this.x + straightLength / 2))
+					+ (y - this.y) * (y - this.y);
+			if (distSquare > outerRadius * outerRadius || distSquare < innerRadius * innerRadius)
+				return false;
+		}
+
 		return true;
 	}
 }
-
