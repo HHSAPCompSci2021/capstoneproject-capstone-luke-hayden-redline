fb3cf9721f5ade5d9d709cc76aae0fbe2226b1d3
Hayden Kaplanov <gkaplanov021@student.fuhsd.org>
2022-05-24 14:18:50 -0700

    Update Main.java

--- CapstoneProject/src/gameplay/Main.java
+++ CapstoneProject/src/gameplay/Main.java
@@ -1,25 +1,37 @@
+// package declarations & imports
 package gameplay;
 import java.awt.Dimension;
+import java.io.IOException;
 import javax.swing.JFrame;
 import processing.awt.PSurfaceAWT;
 import processing.core.PApplet;
 import java.util.Scanner;
+
 public class Main {
+	// creates the width and height fields
 	private static int w;
 	private static int h;
+	
+	// sets up the scanner
 	private static Scanner scan;
-	public static void main(String[] args) {
+	
+	// main method
+	public static void main(String[] args) throws IOException {
+		// asks for window dimensions and gets them
 		scan = new Scanner(System.in);
 		System.out.println("How wide should the window be?");
 		w = scan.nextInt();
 		System.out.println("How tall should the window be?");
 		h = scan.nextInt();
+		
+		// sets up the window
 		DrawingSurface drawing = new DrawingSurface();
 		PApplet.runSketch(new String[]{""}, drawing);
 		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
 		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
 		JFrame window = (JFrame)canvas.getFrame();
-		window.setSize(1300,900);
+		
+		// sizes the window and finishes setting it up
 		window.setSize(w, h);
 		window.setLocation(100, 100);
 		window.setMinimumSize(new Dimension(100,100));
