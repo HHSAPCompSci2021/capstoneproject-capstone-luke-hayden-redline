56fd084971ad238beb8f1fc63a224696c63510d5
Hayden Kaplanov <gkaplanov021@student.fuhsd.org>
2022-05-24 14:09:52 -0700

    what do I name this
    I do not have names for commits anymore

--- CapstoneProject/src/gameplay/Main.java
+++ CapstoneProject/src/gameplay/Main.java
@@ -1,26 +1,30 @@
 package gameplay;
 import java.awt.Dimension;
 import javax.swing.JFrame;
-
 import processing.awt.PSurfaceAWT;
 import processing.core.PApplet;
 import java.util.Scanner;
 public class Main {
-	//private static Scanner scan;
+	private static int w;
+	private static int h;
+	private static Scanner scan;
 	public static void main(String[] args) {
-
+		scan = new Scanner(System.in);
+		System.out.println("How wide should the window be?");
+		w = scan.nextInt();
+		System.out.println("How tall should the window be?");
+		h = scan.nextInt();
 		DrawingSurface drawing = new DrawingSurface();
 		PApplet.runSketch(new String[]{""}, drawing);
 		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
 		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
 		JFrame window = (JFrame)canvas.getFrame();
-
 		window.setSize(1300,900);
+		window.setSize(w, h);
 		window.setLocation(100, 100);
 		window.setMinimumSize(new Dimension(100,100));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(true);
-
 		window.setVisible(true);
 	}
 }
